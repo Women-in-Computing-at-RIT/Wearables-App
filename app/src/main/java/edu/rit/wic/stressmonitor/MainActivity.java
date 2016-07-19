19698cdbf767f478c504dff596641df7007d6632
Cara <cxs4754@rit.edu>
2016-07-19 12:02:37 -0400

    Fixed issue now redirects from login.

--- app/src/main/java/edu/rit/wic/stressmonitor/MainActivity.java
+++ app/src/main/java/edu/rit/wic/stressmonitor/MainActivity.java
@@ -3,7 +3,7 @@ package edu.rit.wic.stressmonitor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -13,16 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        startActivity(new Intent(this, LoginActivity.class));
-
-        Toolbar menu = (Toolbar) findViewById(R.id.menu_bar);
-        setSupportActionBar(menu);
-
 //        If connected to device via Bluetooth
         findViewById(R.id.link_connected_devices).setVisibility(View.VISIBLE);
         findViewById(R.id.bt_icon_green).setVisibility(View.VISIBLE);
         findViewById(R.id.link_disconnected_devices).setVisibility(View.INVISIBLE);
         findViewById(R.id.bt_icon_gray).setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
