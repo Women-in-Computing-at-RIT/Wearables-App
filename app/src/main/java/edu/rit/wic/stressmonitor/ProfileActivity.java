59187e0c81e615f389a82ed5d62c2d30e0fa2351
Cara <cxs4754@rit.edu>
2016-07-22 14:55:05 -0400

    Updated

--- app/src/main/java/edu/rit/wic/stressmonitor/ProfileActivity.java
+++ app/src/main/java/edu/rit/wic/stressmonitor/ProfileActivity.java
@@ -18,7 +18,6 @@ import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 public class ProfileActivity extends AppCompatActivity {
-    Drawer nav_drawer;
     Toolbar toolbar;
     ActionBar actionBar;
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +26,11 @@ public class ProfileActivity extends AppCompatActivity {
 
         // Find the toolbar view inside the activity layout
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white_24dp);
         toolbar.setTitle("My Profile");
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
-
-        // Creates navigation drawer
-//        createNavDrawer();
     }
 
     // Menu icons are inflated just as they were with actionbar
@@ -44,44 +41,15 @@ public class ProfileActivity extends AppCompatActivity {
         return true;
     }
 
-//    public Drawer createNavDrawer() {
-//        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.home_drawer);
-//        PrimaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.profile_drawer);
-//        PrimaryDrawerItem item3 = new SecondaryDrawerItem().withIdentifier(3).withName(R.string.contact_drawer);
-//        nav_drawer = new DrawerBuilder()
-//                .withActivity(this)
-//                .withTranslucentStatusBar(false)
-//                .withActionBarDrawerToggle(true)
-//                .withToolbar(toolbar)
-//                .addDrawerItems(
-//                        item1,
-//                        item2,
-//                        new DividerDrawerItem(),
-//                        item3
-//                )
-//                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-//                    @Override
-//                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-//                        if (drawerItem == item1) {
-//                            startActivity(new Intent(ProfileActivity.this, MainActivity.class));
-//                        } else if (drawerItem == item2) {
-//                            nav_drawer.closeDrawer();
-//                        } else if (drawerItem == item3) {
-//                            startActivity(new Intent(ProfileActivity.this, ContactActivity.class));
-//                        }
-//                        return true;
-//                    }
-//                })
-//                .build();
-//
-//        //Back arrow
-//        nav_drawer.getActionBarDrawerToggle().setDrawerIndicatorEnabled(false);
-//        actionBar.setDisplayHomeAsUpEnabled(true);
-//        // Hamburger icon
-//        actionBar.setDisplayHomeAsUpEnabled(false);
-//        nav_drawer.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
-//        nav_drawer.setSelection(2);
-//
-//        return nav_drawer;
-//    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
